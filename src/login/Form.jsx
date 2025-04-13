41d4681541aecb6e38751ad1a416b754bd701ec2
Jordan S. Arrivado <j0rdanarrivado@gmail.com>
2025-04-13 20:54:33 +0800

    update api

--- src/login/Form.jsx
+++ src/login/Form.jsx
@@ -7,7 +7,7 @@ import { useAuth } from '../AuthContext';
 import { useNavigate } from 'react-router-dom';
 import bgVid from '../videos/bgVid3.mp4';
 
-const API_URL = 'http://localhost:3001'; 
+const API_URL = 'https://bqms-backend.onrender.com'; 
 
 const LoginPage = () => {
   const [isSignUp, setIsSignUp] = useState(false);
